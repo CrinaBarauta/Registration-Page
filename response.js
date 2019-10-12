5d7f24277f7c974b3e146abfd4de431fa23de102
CrinaBarauta <44180111+CrinaBarauta@users.noreply.github.com>
2019-10-12 17:58:02 +0300

    Add files via upload

---
+++ response.js
@@ -0,0 +1,30 @@
+var _formLang,
+_defaultBtnText,
+_errorMessages= {
+    required: {
+        en: "This field is required"
+    }
+    ,
+    name: {
+        en:"Please use Latin characters and digits only, special symbols (/“”(){}[]#$&*+,) are not supported"
+    }
+    ,
+    email: {
+        en: "Must be valid email address. example@domain.com"
+    }
+    ,
+    business: {
+        en: "Please enter your business email"
+    }
+    ,
+    phone: {
+        en: "Must be valid phone number. 123-456-78-90"
+    }
+}
+
+,
+_GDPRText= {
+    en: {
+        privacy_policy: 'By registering you agree to the&nbsp;<a href="#" target="_blank">Privacy Policy</a>.', optout: "I want to receive product news and&nbsp;updates."
+    }
+}
